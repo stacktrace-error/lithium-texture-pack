b9573948028fa1a8fbbd6e5ff6a9a4b387b0de9b
stacktrace-error <89076920+stacktrace-error@users.noreply.github.com>
2021-10-15 04:13:13 +0200

    Add files via upload

---
+++ scripts/Main.js
@@ -0,0 +1,25 @@
+Pal.heal = Color.valueOf("d0d0e0");
+
+//nova bolts
+UnitTypes.nova.weapons.get(0).bullet.backColor.set(Color.valueOf("d0d0e0"));
+UnitTypes.nova.weapons.get(1).bullet.backColor.set(Color.valueOf("d0d0e0"));
+
+//quasar lasers
+UnitTypes.quasar.weapons.get(0).bullet.colors[0].set(Color.valueOf("d0d0e066"));
+UnitTypes.quasar.weapons.get(1).bullet.colors[0].set(Color.valueOf("d0d0e066"));
+
+//vela laser
+UnitTypes.vela.weapons.get(0).bullet.colors[0].set(Color.valueOf("d0d0e066"));
+UnitTypes.vela.weapons.get(0).bullet.colors[1].set(Color.valueOf("d0d0e0"));
+
+//corvus laser
+UnitTypes.corvus.weapons.get(0).bullet.lightningColor = Color.valueOf("d0d0e0");
+
+UnitTypes.corvus.weapons.get(0).bullet.colors[0].set(Color.valueOf("d0d0e066"));
+UnitTypes.corvus.weapons.get(0).bullet.colors[1].set(Color.valueOf("d0d0e0"));
+
+//mega bolts
+UnitTypes.mega.weapons.get(0).bullet.backColor.set(Color.valueOf("d0d0e0"));
+UnitTypes.mega.weapons.get(1).bullet.backColor.set(Color.valueOf("d0d0e0"));
+UnitTypes.mega.weapons.get(2).bullet.backColor.set(Color.valueOf("d0d0e0"));
+UnitTypes.mega.weapons.get(3).bullet.backColor.set(Color.valueOf("d0d0e0"));
